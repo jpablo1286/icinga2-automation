928086d35c197572f4027fedc4e7489e75764713
Juan Pablo Rivera Velasco <jpablo.localhost@gmail.com>
2021-03-17 21:25:50 -0500

    Adding playbook for install and setup icinga2 and icingaweb2

--- ansible/templates/clean.sh
+++ ansible/templates/clean.sh
@@ -2,7 +2,7 @@
 yumcount=`ps aux | grep yum | wc -l`
 while [ $yumcount -gt 1 ]
 do
-kill -9 `ps aux | grep yum | grep upgrade | head -1 | awk '{ print $2 }'` || true
+kill -15 `ps aux | grep yum | grep upgrade | head -1 | awk '{ print $2 }'` || true
 sleep 5s
 yumcount=`ps aux | grep yum | wc -l`
 done
